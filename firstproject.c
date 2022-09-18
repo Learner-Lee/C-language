306e5aac8566546d42146987dafbf9ca79a9a763
Learner-Lee <89835879+Learner-Lee@users.noreply.github.com>
2022-09-18 21:37:05 +0800

    Add files via upload

--- firstproject.c
+++ firstproject.c
@@ -1,6 +1,13 @@
-# include <stdio.h>
+# include <stdio.h>//引入头文件；使用printf函数；必不可少。
 int main()
 {
 	printf("Hello World\n");
+	int a=add(2, 3);
+	printf("%d",a);
 	return 0;
+}
+int add(int a, int b)
+{
+	return a + b;
+
 }
