881efeca4e11a20c15dfce9fcb1a5d765c65a2bb
xiangqianLi <89835879+Learner-Lee@users.noreply.github.com>
2023-09-04 10:45:54 +0800

    Add files via upload

--- class 01/change-Defining-Constants.c
+++ class 01/change-Defining-Constants.c
@@ -3,11 +3,14 @@
 int main(){
 
     // 定义常量,如果不定义就是magic number（你不知道他是干什么的）
-    const int AMOUNT = 100;
+    // const 变量名最好全大写
+    const int AMOUNT = 100; // const 表示变量可以赋初值但不能修改
     int price = 0;
 
-    printf("请输入金额（元）：");
+    // AMOUNT = 90;  不可被赋值
 
+    printf("请输入金额（元）：");
+ 
     scanf("%d" , &price);
 
     int change = AMOUNT - price;
